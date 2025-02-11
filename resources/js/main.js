e49cd139b1db1c497ed5bcc49278d793f6852859
tedodorglisic <teodor.glisic13@icloud.com>
2025-02-11 10:27:50 +0100

    Added js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,5 @@
 function expandCollapsible() {
     var links = document.getElementById("link_items").style;
-    console.log("works")
     if (links.display == "flex") {
         links.display = "none";
     } else {
